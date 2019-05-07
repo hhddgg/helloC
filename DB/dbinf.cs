6d6e2eb4964e8bb7167ddc463a8a5d0c413fd225
hhdg <xctech@outlook.com>
2019-05-07 16:53:46 +0800

    first add

---
+++ DB/dbinf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    interface dbinf
+    {
+        bool insertCarinfo(string tablename,string carno,string userid,int count,string carid);
+        bool insertContactinfo(string tablename,string userid,string carno,DateTime times);
+        bool insertrfidman(string tablename,string cardid,string userid,string carno,string status);
+
+
+        
+    }
+}
